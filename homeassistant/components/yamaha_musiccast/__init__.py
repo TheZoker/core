15078808395d789562fdb87648ada27a498b3813
Florian Gareis <mail@zoker.me>
2021-02-27 12:09:33 +0100

    Add yamaha config flow

--- homeassistant/components/yamaha_musiccast/__init__.py
+++ homeassistant/components/yamaha_musiccast/__init__.py
@@ -1,1 +1,68 @@
-"""The yamaha_musiccast component."""
+"""The Yamaha MusicCast integration."""
+import asyncio
+
+import voluptuous as vol
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                MP_DOMAIN: vol.Schema(
+                    {
+                        vol.Optional(CONF_ADVERTISE_ADDR): cv.string,
+                        vol.Optional(CONF_INTERFACE_ADDR): cv.string,
+                        vol.Optional(CONF_HOSTS): vol.All(
+                            cv.ensure_list_csv, [cv.string]
+                        ),
+                    }
+                )
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Yamaha MusicCast component."""
+    conf = config.get(DOMAIN)
+
+    hass.data[DOMAIN] = conf or {}
+
+    if conf is not None:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": config_entries.SOURCE_IMPORT}
+            )
+        )
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Yamaha MusicCast from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, MP_DOMAIN)
+    )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
